72eac57a26de004ad20f9d17ec54a57c4bdc35ad
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 11:06:18 +0200

    starter: 05-transforms

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
 
 function App() {
   return (
-    <Canvas camera={{ position: [3, 3, 3] }}>
-      <OrbitControls />
-      <mesh position-x={-0.6}>
-        <boxGeometry />
-        <meshStandardMaterial color="orange" side={THREE.FrontSide} />
-      </mesh>
-      <mesh position-x={0.6} position-z={1}>
-        <boxGeometry />
-        <meshStandardMaterial color="orange" side={THREE.BackSide} />
-      </mesh>
+    <Canvas camera={{ position: [0, 3, 8] }}>
+      {/* Lighting */}
       <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 0, 3]} intensity={1} />
-      <directionalLight position={[0, 3, 3]} intensity={1} />
+      <directionalLight position={[0, 3, 5]} intensity={0.5} />
+
+      {/* Objects */}
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="red" />
+      </mesh>
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="blue" />
+      </mesh>
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="green" />
+      </mesh>
     </Canvas>
   );
 }
