7baa4bc3e88da0e095ac4901c5a48a338e2e55ba
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 11:16:04 +0200

    starter: 06-camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
+import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-
+import { City } from "./components/City";
 
 function App() {
   return (
-    <Canvas camera={{ position: [0, 3, 8] }}>
-      {/* Lighting */}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 3, 5]} intensity={0.5} />
-
-      {/* Objects */}
-      <group position={[-2, -2, 0]} scale={2} rotation-y={Math.PI / 4}>
-        <mesh position-x={-1}>
-          <boxGeometry />
-          <meshStandardMaterial color="red" />
-        </mesh>
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial color="blue" />
-        </mesh>
-        <mesh position-x={1}>
-          <boxGeometry />
-          <meshStandardMaterial color="green" />
-        </mesh>
-      </group>
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      {/* IGNORE FOR NOW */}
+      <City />
+      <Environment preset="city" />
     </Canvas>
   );
 }
