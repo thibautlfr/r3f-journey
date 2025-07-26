e33462c9d10bd8dc772094336855548df601191f
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-27 00:42:36 +0200

    SoftShadows, BakeShadows, ContactShadows, AccumulativeShadows with RandomizedLight

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,91 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  AccumulativeShadows,
+  OrbitControls,
+  RandomizedLight,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useControls } from "leva";
 
 function App() {
+  const { cubeInAir } = useControls({
+    cubeInAir: false,
+  });
+
   return (
     <>
       <Canvas
         camera={{ position: [0, 3, 3] }}
         shadows
       >
+        {/* Every seconds update shadow map */}
+        {/* <SoftShadows /> */}
+
+        {/* Only on first render  */}
+        {/* <BakeShadows /> */}
+
+        {/* Doesn't need shadow on canvas, doesn't use lights */}
+        {/* <ContactShadows
+          frames={1}
+          position-y={-0.49}
+          opacity={0.5}
+          blur={2}
+          color={"pink"}
+          scale={10}
+        /> */}
+
+        {/* AccumulativeShadows is a performant way to render shadows
+        It uses a randomized light source to create a more natural shadow effect.
+        It can be used with temporal anti-aliasing to create smooth shadows. */}
+        <AccumulativeShadows
+          temporal
+          frames={35}
+          alphaTest={0.85}
+          scale={5}
+          position={[0, -0.499, 0]}
+          color="#EFBD4E"
+        >
+          <RandomizedLight
+            amount={4}
+            radius={9}
+            intensity={0.55}
+            ambient={0.25}
+            position={[5, 5, -10]}
+          />
+          <RandomizedLight
+            amount={4}
+            radius={9}
+            intensity={0.25}
+            ambient={0.55}
+            position={[-5, 5, -9]}
+          />
+        </AccumulativeShadows>
+
         <OrbitControls />
         <ambientLight intensity={0.5} />
         <directionalLight
           position={[5, 5, 5]}
           intensity={0.5}
           castShadow
-          // shadow-camera-far={50}
-          // shadow-camera-left={-10}
-          // shadow-camera-right={10}
-          // shadow-camera-top={10}
-          // shadow-camera-bottom={-10}
-        >
-          <orthographicCamera
-            // now we can use ref to access the camera and tweak value with Leva
-            attach={"shadow-camera"}
-            args={[-10, 10, 10, -10]}
-          />
-        </directionalLight>
+        />
         <directionalLight
           position={[-5, 5, 5]}
           intensity={0.5}
           color="red"
           castShadow
         />
-
         <mesh position={[1, 1, 1]} castShadow>
           <sphereGeometry args={[0.5, 32, 32]} />
           <meshStandardMaterial color="white" />
         </mesh>
-
         <mesh
           rotation-y={Math.PI / 4}
           castShadow
           receiveShadow
+          position-y={cubeInAir ? 1 : 0}
         >
           <boxGeometry />
           <meshStandardMaterial color="white" />
         </mesh>
-
         <mesh
           rotation-x={-Math.PI / 2}
           position-y={-0.5}
@@ -55,33 +94,6 @@ function App() {
           <planeGeometry args={[5, 5]} />
           <meshStandardMaterial color="white" />
         </mesh>
-
-        <group position-x={10}>
-          <mesh position={[1, 1, 1]} castShadow>
-            <sphereGeometry
-              args={[0.5, 32, 32]}
-            />
-            <meshStandardMaterial color="white" />
-          </mesh>
-
-          <mesh
-            rotation-y={Math.PI / 4}
-            castShadow
-            receiveShadow
-          >
-            <boxGeometry />
-            <meshStandardMaterial color="white" />
-          </mesh>
-
-          <mesh
-            rotation-x={-Math.PI / 2}
-            position-y={-0.5}
-            receiveShadow
-          >
-            <planeGeometry args={[5, 5]} />
-            <meshStandardMaterial color="white" />
-          </mesh>
-        </group>
       </Canvas>
     </>
   );
