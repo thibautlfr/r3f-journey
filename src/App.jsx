67de6a72e3e93093d9e4ad0f12d5ca4bdf418668
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 11:14:16 +0200

    group elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Canvas } from "@react-three/fiber";
 
-import * as THREE from "three";
 
 function App() {
   return (
@@ -10,27 +9,20 @@ function App() {
       <directionalLight position={[0, 3, 5]} intensity={0.5} />
 
       {/* Objects */}
-      <mesh
-        position={[-3, 0, 0]}
-        scale={[0.5, 0.5, 0.5]}
-        rotation={[THREE.MathUtils.degToRad(45), 0, 0]}
-      >
-        <boxGeometry />
-        <meshStandardMaterial color="red" />
-      </mesh>
-      <mesh position={[0, 0, 0]} rotation-y={Math.PI / 4}>
-        <boxGeometry />
-        <meshStandardMaterial color="blue" />
-      </mesh>
-      <mesh
-        position={[3, 0, 0]}
-        scale={[2, 2, 2]}
-        rotation-y={Math.PI / 4}
-        rotation-z={THREE.MathUtils.degToRad(30)}
-      >
-        <boxGeometry />
-        <meshStandardMaterial color="green" />
-      </mesh>
+      <group position={[-2, -2, 0]} scale={2} rotation-y={Math.PI / 4}>
+        <mesh position-x={-1}>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        <mesh position-x={1}>
+          <boxGeometry />
+          <meshStandardMaterial color="green" />
+        </mesh>
+      </group>
     </Canvas>
   );
 }
