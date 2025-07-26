c35569d8c012993f3746720dd1b9d303c2b3a4a1
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 11:39:08 +0200

    helpers, leva ui, stats and perfs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
+import {
+  Grid,
+  Stats,
+  useHelper,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Leva, useControls } from "leva";
+import { useRef } from "react";
+
+import * as THREE from "three";
 
 const Box = () => {
+  const ref = useRef();
+  useHelper(ref, THREE.BoxHelper, "red");
+
+  const {
+    position,
+    color,
+    transparent,
+    opacity,
+  } = useControls({
+    position: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    color: "#ff0000",
+    opacity: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    transparent: true,
+  });
   return (
-    <mesh>
+    <mesh
+      ref={ref}
+      position={[
+        position.x,
+        position.y,
+        position.z,
+      ]}
+    >
       <boxGeometry />
       <meshBasicMaterial
-        color="white"
-        transparent
-        opacity={0}
+        color={color}
+        side={THREE.DoubleSide}
+        transparent={transparent}
+        opacity={opacity}
       />
     </mesh>
   );
@@ -16,7 +56,24 @@ const Box = () => {
 function App() {
   return (
     <>
+      <Leva hidden />
+      <Stats />
       <Canvas camera={{ position: [3, 3, 3] }}>
+        <axesHelper />
+        {/* <gridHelper
+          args={[10, 10, "green", "blue"]}
+        /> */}
+        <Grid
+          sectionSize={3}
+          sectionColor={"purple"}
+          sectionThickness={1}
+          cellSize={1}
+          cellColor={"#6f6f6f"}
+          cellThickness={0.6}
+          infiniteGrid
+          fadeDistance={50}
+          fadeStrength={5}
+        />
         <Box />
       </Canvas>
     </>
