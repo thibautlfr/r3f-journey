8764866c6f6dcb6b31fa75ada12f88b292dadb91
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 11:02:09 +0200

    learn mesh and geometry

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
 
 function App() {
   return (
-    <Canvas camera={{ position: [0, 3, 3] }}>
-      <mesh>
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      <OrbitControls />
+      <mesh position-x={-0.6}>
         <boxGeometry />
-        <meshNormalMaterial />
+        <meshStandardMaterial color="orange" side={THREE.FrontSide} />
       </mesh>
+      <mesh position-x={0.6} position-z={1}>
+        <boxGeometry />
+        <meshStandardMaterial color="orange" side={THREE.BackSide} />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 3]} intensity={1} />
+      <directionalLight position={[0, 3, 3]} intensity={1} />
     </Canvas>
   );
 }
