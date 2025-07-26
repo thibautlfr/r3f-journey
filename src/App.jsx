c6d7474bfc94c951ff0485f5f2a2355394b219b0
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 11:21:54 +0200

    perspective camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { Environment } from "@react-three/drei";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { City } from "./components/City";
 
 function App() {
   return (
-    <Canvas camera={{ position: [3, 3, 3] }}>
+    <Canvas>
+      <PerspectiveCamera
+        position={[3, 3, 3]}
+        makeDefault
+        near={0.1}
+        far={1000}
+      />
+      <OrbitControls />
       {/* IGNORE FOR NOW */}
       <City />
       <Environment preset="city" />
