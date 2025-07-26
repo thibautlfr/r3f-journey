38a2bf7d7a544f4b8e805a52273151feb5113312
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 12:02:33 +0200

    starter: 09-shadows

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,23 @@
-import {
-  OrbitControls,
-  SpotLight,
-  useHelper,
-} from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useControls } from "leva";
-import { useRef } from "react";
-import * as THREE from "three";
-
-const Lights = () => {
-  const ref = useRef();
-  const helper = useHelper(
-    ref,
-    THREE.SpotLightHelper,
-    "red"
-  );
-
-  const {
-    color,
-    distance,
-    attenuation,
-    angle,
-    anglePower,
-  } = useControls({
-    color: "#876ae5",
-    distance: 6,
-    attenuation: 2.2,
-    angle: 1,
-    anglePower: 1,
-  });
-
-  return (
-    <SpotLight
-      ref={ref}
-      color={color}
-      distance={distance}
-      angle={angle}
-      penumbra={0.1}
-      attenuation={attenuation}
-      anglePower={anglePower}
-    />
-  );
-};
 
 function App() {
   return (
     <>
       <Canvas camera={{ position: [0, 3, 3] }}>
         <OrbitControls />
-
-        <Lights />
-        {/* <ambientLight intensity={0.2} /> */}
-        {/* <directionalLight
-          intensity={0.5}
-          position={[3, 3, 3]}
-          color="red"
-        />
-        <directionalLight
-          intensity={0.5}
-          position={[0, 3, -3]}
-          color="green"
-        />
-        <directionalLight
-          intensity={0.5}
-          position={[-3, 3, 3]}
-          color="blue"
-        /> */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} intensity={0.5} />
+        <directionalLight position={[-5, 5, 5]} intensity={0.5} color="red" />
 
         <mesh rotation-y={Math.PI / 4}>
           <boxGeometry />
-          <meshStandardMaterial
-            color="white"
-            roughness={1}
-            metalness={0}
-          />
+          <meshStandardMaterial color="white" />
         </mesh>
 
-        <mesh
-          rotation-x={-Math.PI / 2}
-          position-y={-0.5}
-        >
+        <mesh rotation-x={-Math.PI / 2} position-y={-0.5}>
           <planeGeometry args={[5, 5]} />
-          {/* <meshStandardMaterial
-            color="white"
-            roughness={0.2}
-            metalness={0.8}
-          /> */}
-          <meshPhysicalMaterial
-            color="white"
-            clearcoat={0.5}
-            reflectivity={0.8}
-          />
+          <meshStandardMaterial color="white" />
         </mesh>
       </Canvas>
     </>
