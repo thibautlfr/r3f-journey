a2151cbd573f631aa214c1955138590ac1e7f51c
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 11:12:09 +0200

    position, scale, rotation, degToRad

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 
+import * as THREE from "three";
+
 function App() {
   return (
     <Canvas camera={{ position: [0, 3, 8] }}>
@@ -8,15 +10,24 @@ function App() {
       <directionalLight position={[0, 3, 5]} intensity={0.5} />
 
       {/* Objects */}
-      <mesh position={[0, 0, 0]}>
+      <mesh
+        position={[-3, 0, 0]}
+        scale={[0.5, 0.5, 0.5]}
+        rotation={[THREE.MathUtils.degToRad(45), 0, 0]}
+      >
         <boxGeometry />
         <meshStandardMaterial color="red" />
       </mesh>
-      <mesh position={[0, 0, 0]}>
+      <mesh position={[0, 0, 0]} rotation-y={Math.PI / 4}>
         <boxGeometry />
         <meshStandardMaterial color="blue" />
       </mesh>
-      <mesh position={[0, 0, 0]}>
+      <mesh
+        position={[3, 0, 0]}
+        scale={[2, 2, 2]}
+        rotation-y={Math.PI / 4}
+        rotation-z={THREE.MathUtils.degToRad(30)}
+      >
         <boxGeometry />
         <meshStandardMaterial color="green" />
       </mesh>
