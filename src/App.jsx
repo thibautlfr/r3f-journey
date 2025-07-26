7e04a586645439c6423ecb68cf88ee0f2bdaea40
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-26 11:40:47 +0200

    starter: 08-lights

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,20 @@
-import {
-  Grid,
-  Stats,
-  useHelper,
-} from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Leva, useControls } from "leva";
-import { useRef } from "react";
-
-import * as THREE from "three";
-
-const Box = () => {
-  const ref = useRef();
-  useHelper(ref, THREE.BoxHelper, "red");
-
-  const {
-    position,
-    color,
-    transparent,
-    opacity,
-  } = useControls({
-    position: {
-      x: 0,
-      y: 0,
-      z: 0,
-    },
-    color: "#ff0000",
-    opacity: {
-      value: 0.5,
-      min: 0,
-      max: 1,
-      step: 0.01,
-    },
-    transparent: true,
-  });
-  return (
-    <mesh
-      ref={ref}
-      position={[
-        position.x,
-        position.y,
-        position.z,
-      ]}
-    >
-      <boxGeometry />
-      <meshBasicMaterial
-        color={color}
-        side={THREE.DoubleSide}
-        transparent={transparent}
-        opacity={opacity}
-      />
-    </mesh>
-  );
-};
-
 function App() {
   return (
     <>
-      <Leva hidden />
-      <Stats />
-      <Canvas camera={{ position: [3, 3, 3] }}>
-        <axesHelper />
-        {/* <gridHelper
-          args={[10, 10, "green", "blue"]}
-        /> */}
-        <Grid
-          sectionSize={3}
-          sectionColor={"purple"}
-          sectionThickness={1}
-          cellSize={1}
-          cellColor={"#6f6f6f"}
-          cellThickness={0.6}
-          infiniteGrid
-          fadeDistance={50}
-          fadeStrength={5}
-        />
-        <Box />
+      <Canvas camera={{ position: [0, 3, 3] }}>
+        <OrbitControls />
+
+        <mesh rotation-y={Math.PI / 4}>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+
+        <mesh rotation-x={-Math.PI / 2} position-y={-0.5}>
+          <planeGeometry args={[5, 5]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
       </Canvas>
     </>
   );
