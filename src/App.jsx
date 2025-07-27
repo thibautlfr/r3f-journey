00095d566ae282e488ac087eb14c5a6a15225bab
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-27 13:32:06 +0200

    starter: 15-models-3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
-import { KeyboardControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useMemo } from "react";
 import { Experience } from "./components/Experience";
-
-export const Controls = {
-  forward: "forward",
-  back: "back",
-  left: "left",
-  right: "right",
-};
-
 function App() {
-  const map = useMemo(() => [
-    {
-      name: Controls.forward,
-      keys: ["ArrowUp", "KeyW"],
-    },
-    {
-      name: Controls.back,
-      keys: ["ArrowDown", "KeyS"],
-    },
-    {
-      name: Controls.left,
-      keys: ["ArrowLeft", "KeyA"],
-    },
-    {
-      name: Controls.right,
-      keys: ["ArrowRight", "KeyD"],
-    },
-  ]);
   return (
     <>
-      <KeyboardControls map={map}>
-        <Canvas
-          camera={{
-            position: [0, 1, 8],
-            fov: 42,
-          }}
-        >
-          <Experience />
-        </Canvas>
-      </KeyboardControls>
+      <Canvas camera={{ position: [-1.5, 3, 10], fov: 42 }}>
+        <Experience />
+        <OrbitControls />
+        <Environment preset="sunset" />
+      </Canvas>
     </>
   );
 }
