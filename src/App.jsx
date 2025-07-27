91b35e7672a4108d951ac18243bd6017b5980c97
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-27 13:10:31 +0200

    PivotControls and custom it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import {
-  Environment,
-  PresentationControls,
-} from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Lighthouse } from "./components/Lighthouse";
+import { MoveableItem } from "./components/MoveableItem";
 function App() {
   return (
     <>
@@ -13,27 +11,13 @@ function App() {
           fov: 42,
         }}
       >
-        <PresentationControls
-          enabled={true} // the controls can be disabled by setting this to false
-          global={false} // Spin globally or by dragging the model
-          cursor={true} // Whether to toggle cursor style on drag
-          snap={true} // Snap-back to center (can also be a spring config)
-          speed={1} // Speed factor
-          zoom={1} // Zoom factor when half the polar-max is reached
-          rotation={[0, 0, 0]} // Default rotation
-          polar={[0, Math.PI / 2]} // Vertical limits
-          azimuth={[-Infinity, Infinity]} // Horizontal limits
-          config={{
-            mass: 1,
-            tension: 170,
-            friction: 26,
-          }} // Spring config
-        >
+        <MoveableItem>
           <Lighthouse
             position-y={-1}
             scale={[0.2, 0.2, 0.2]}
           />
-        </PresentationControls>
+        </MoveableItem>
+
         <Environment preset="sunset" />
       </Canvas>
     </>
