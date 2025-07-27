ce08160970b81d7ccd81815d756225838e2e9415
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-07-27 13:28:58 +0200

    pointer, click, useCursor, keyBoardControls

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
+import { KeyboardControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useMemo } from "react";
 import { Experience } from "./components/Experience";
 
+export const Controls = {
+  forward: "forward",
+  back: "back",
+  left: "left",
+  right: "right",
+};
+
 function App() {
+  const map = useMemo(() => [
+    {
+      name: Controls.forward,
+      keys: ["ArrowUp", "KeyW"],
+    },
+    {
+      name: Controls.back,
+      keys: ["ArrowDown", "KeyS"],
+    },
+    {
+      name: Controls.left,
+      keys: ["ArrowLeft", "KeyA"],
+    },
+    {
+      name: Controls.right,
+      keys: ["ArrowRight", "KeyD"],
+    },
+  ]);
   return (
     <>
-      <Canvas camera={{ position: [0, 3, 8], fov: 42 }}>
-        <Experience />
-      </Canvas>
+      <KeyboardControls map={map}>
+        <Canvas
+          camera={{
+            position: [0, 1, 8],
+            fov: 42,
+          }}
+        >
+          <Experience />
+        </Canvas>
+      </KeyboardControls>
     </>
   );
 }
--- src/components/MoveableSphere.jsx
+++ src/components/MoveableSphere.jsx
@@ -1,8 +1,71 @@
+import {
+  useCursor,
+  useKeyboardControls,
+} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { Controls } from "../App";
+
+const MOVEMENT_SPEED = 0.05;
+
 export const MoveableSphere = (props) => {
+  const [hovered, setHovered] = useState(false);
+  const [selected, setSelected] = useState(false);
+  let color = hovered ? "pink" : "white";
+  useCursor(hovered);
+  const ref = useRef();
+
+  const forwardPressed = useKeyboardControls(
+    (state) => state[Controls.forward]
+  );
+  const backPressed = useKeyboardControls(
+    (state) => state[Controls.back]
+  );
+  const leftPressed = useKeyboardControls(
+    (state) => state[Controls.left]
+  );
+  const rightPressed = useKeyboardControls(
+    (state) => state[Controls.right]
+  );
+
+  useFrame(() => {
+    if (!selected) return;
+
+    if (forwardPressed) {
+      ref.current.position.y += MOVEMENT_SPEED;
+    } else if (backPressed) {
+      ref.current.position.y -= MOVEMENT_SPEED;
+    } else if (leftPressed) {
+      ref.current.position.x -= MOVEMENT_SPEED;
+    } else if (rightPressed) {
+      ref.current.position.x += MOVEMENT_SPEED;
+    }
+  });
+
+  if (selected) {
+    color = "hotpink";
+  }
+
   return (
-    <mesh {...props}>
+    <mesh
+      ref={ref}
+      {...props}
+      onPointerEnter={(e) => {
+        e.stopPropagation();
+        setHovered(true);
+      }}
+      onPointerLeave={(e) => {
+        e.stopPropagation();
+        setHovered(false);
+      }}
+      onClick={(e) => {
+        e.stopPropagation();
+        setSelected(!selected);
+      }}
+      onPointerMissed={() => setSelected(false)}
+    >
       <sphereGeometry args={[0.5, 64, 64]} />
-      <meshStandardMaterial color={"white"} />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
