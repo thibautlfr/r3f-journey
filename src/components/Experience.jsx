4424331d31a2c261c6066a14a2c3c9896caa3fb2
thibautlfr <thibaut.lfr@gmail.com>
2026-05-16 13:30:41 +0200

    Add Fish component and integrate GLTF models into Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,16 @@
+import {useFBX, useGLTF} from "@react-three/drei";
+import {Fish} from "./Fish.jsx";
+
 export const Experience = () => {
+  const {scene} = useGLTF("models/Fish.gltf");
+  const dino = useFBX("models/Dino.fbx");
   return (
     <>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="white" />
-      </mesh>
+      <ambientLight intensity={1}/>
+      {/*<primitive object={scene}/>*/}
+      {/*<Gltf src="models/Fish.gltf"/>*/}
+      <Fish/>
+      <primitive object={dino} scale={0.01} position-x={-3}/>
     </>
   );
 };
